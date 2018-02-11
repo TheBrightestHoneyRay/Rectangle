5d1367248727dcca2a6aa8b38faaa9e7de2f87c0
Balnur <balnurquarter18@gmail.com>
2018-02-11 12:13:03 +0600

    First commit

---
+++ Rectangle1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle1
+{
+    class Rectangle
+    {
+        public int width;
+        public int height;
+
+        public Rectangle(int a, int b)
+        {
+            width = a;
+            height = b;
+        }
+
+        public int FindArea()
+        {
+            return width * height;
+        }
+
+        public int FindPerim()
+        {
+            return (width + height) * 2; 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Rectangle r = new Rectangle(a, b);
+            Console.WriteLine(r.FindArea() + " " + r.FindPerim());
+            
+            Console.ReadKey();
+        }
+
+
+    }
+}
